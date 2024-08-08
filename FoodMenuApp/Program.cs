ecb881214135915b6cb921b0ba18641a378493e5
justinscottjenecke <justinjenecke@gmail.com>
2024-08-08 09:08:32 +0200

    configure: modifies sql connection string

--- FoodMenuApp/Program.cs
+++ FoodMenuApp/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<FoodMenuContext>(options => {
-    options.UseSqlServer("Server=DESKTOP-E1M122C;Database=FccFoodMenuDB;Trusted_Connection=True;");
+    options.UseSqlServer("Server=DESKTOP-E1M122C; Database=FccFoodMenuDB; Trusted_Connection=True; TrustServerCertificate=True;");
 });
 
 var app = builder.Build();
